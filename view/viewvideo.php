3d8c5db313f9d7bd26a6db99d4907a4e87127524
lovecls1 <lovecls1@gmail.com>
2013-09-19 22:49:18 +0530

    some changes

--- view/viewvideo.php
+++ view/viewvideo.php
@@ -1,3 +1,8 @@
+<?php
+    require 'chkSession.php';
+
+?>
+
 <!DOCTYPE HTML>
 
 <head>
@@ -7,7 +12,22 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     
 <!-- Google Fonts -->
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+   body {
+      font-family: "myriad pro", tahoma, verdana, arial, sans-serif;
+      font-size: 14px;
+      margin: 0;
+      padding: 0;
+   }
+   #content {
+      margin: 50px auto;
+      max-width: 982px;
+   }
+      </style>
+
+   <!-- The "minimalist" skin, "minimalist.css", "functional.css" and "playful.css" are available -->
+   <link rel="stylesheet" href="//releases.flowplayer.org/5.4.3/skin/minimalist.css">
     <link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
 
 <!-- CSS Files -->
@@ -18,6 +38,24 @@
     <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen"/>
 
 <!-- JS Files -->
+ <!-- Flowplayer depends on jquery -->
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+   <!-- Flowplayer-->
+   <script src="//releases.flowplayer.org/5.4.3/flowplayer.min.js"></script>
+
+   <script>
+   // configure rtmp for this demo
+   flowplayer.conf.rtmp = "rtmp://s3b78u0kbtx79q.cloudfront.net/cfx/st";
+   // install player manually
+   $(function() {
+      $(".player").flowplayer({
+         // reverse fraction of video aspect ratio
+         // video dimensions: 470px / 250px
+         ratio: 25/47
+      });
+   });
+</script>
 
 	<script type="text/javascript" src="js/jquery.min.js"></script>
     
@@ -45,8 +83,7 @@
     <?php
 include "header.php";
 ?><!-- END header -->
-
-
+ 
     <div id="container">
 
 
@@ -56,12 +93,23 @@ include "header.php";
 	  </div>
 	  
 		<a href="myprofile.php">Back to Home</a>
-                <form name="f2" method="post" action="../controller/UploadFileController.php" enctype="multipart/form-data" >
+                <form name="f2" method="post" action="../controller/UpdateBioController.php" enctype="multipart/form-data" >
       <table width="100%" border="1">
         <tr align="center">
           <td align="center" style="padding:20px">
-		  <p>View Video</p>
-		  
+		 <div id="content">
+
+                <div class="player ">
+                    <video>
+                     <source type="video/webm"  src="http://stream.flowplayer.org/black/470x250.webm">
+                    <source type="video/mp4"   src="http://abpnews.newsbullet.in/livetv/testvideo.mp4">
+                     <source type="video/ogg"   src="http://stream.flowplayer.org/black/470x250.ogv">
+                     <source type="video/flash" src="mp4:black/470x250.mp4">   
+                    </video>
+                </div>
+
+                </div><!--/end content -->
+
 		  </td>
         <tr>
          <tr>
